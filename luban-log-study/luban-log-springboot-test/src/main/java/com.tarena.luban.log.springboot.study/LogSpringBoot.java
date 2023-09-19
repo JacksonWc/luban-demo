7724469ab40ba57390233dd7662d2a06470a6479
hebaodan <19560122127@163.com>
2023-09-19 17:09:32 +0800

    自定义SpringBoot启动类

---
+++ luban-log-study/luban-log-springboot-test/src/main/java/com.tarena.luban.log.springboot.study/LogSpringBoot.java
@@ -0,0 +1,25 @@
+package com.tarena.luban.log.springboot.study;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+/**
+ * springboot应用核心注解
+ */
+@SpringBootApplication
+public class LogSpringBoot {
+    //获取日志对象
+    private static final Logger logger=
+            LoggerFactory.getLogger(LogSpringBoot.class);
+    //args是什么？ jvm参数
+    public static void main(String[] args) {
+        //运行spring应用
+        SpringApplication.run(LogSpringBoot.class,args);
+        //打印一些日志
+        logger.info("获取jvm 参数："+Arrays.toString(args));
+    }
+}
