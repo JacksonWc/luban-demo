31398c5e5862fe80ed3ba91b2449970bd2108b01
hebaodan <19560122127@163.com>
2023-09-20 14:03:12 +0800

    luban-demo 单体架构转向纵向拆分，搭建目录结构后自建立依赖关系，添加每个main项目自己特有的依赖并粘贴代码

---
+++ luban-demo-cart/luban-demo-cart-main/src/main/java/com/tarena/demo/luban/all/main/config/LocalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.tarena.demo.luban.all.main.config;
+
+
+import commons.exception.handler.GlobalControllerExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("com.tarena.demo.luban.all.main.controller")
+public class LocalExceptionHandler extends GlobalControllerExceptionHandler {
+}
---
+++ luban-demo-order/luban-demo-order-main/src/main/java/com/tarena/demo/luban/all/main/config/MybatisConfig.java
@@ -0,0 +1,10 @@
+package com.tarena.demo.luban.all.main.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.tarena.demo.luban.all.main.mapper")
+public class MybatisConfig {
+
+}
