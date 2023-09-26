c2deb5da02ec2ca24a57756c8bd95da992a44270
hebaodan <19560122127@163.com>
2023-09-26 16:14:18 +0800

    luban-demo  spring cloud gateway：过滤器--控制请求

--- luban-demo-gateway/src/main/java/luban/demo/gateway/filter/FirstFilter.java
+++ luban-demo-gateway/src/main/java/luban/demo/gateway/filter/FirstFilter.java
@@ -3,9 +3,16 @@ package luban.demo.gateway.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.RequestPath;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.List;
 
 
 @Component
@@ -21,9 +28,36 @@ public class FirstFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         System.out.println("进入到firstFilter,但是什么都没做");
-        //过滤链调用后续过滤逻辑
-        Mono<Void> mono = chain.filter(exchange);
-        return mono;
+        //System.out.println("进入到firstFilter,但是什么都没做");
+        //request对象 包含请求url地址 path 请求query参数 请求头数据
+        //从exchange拿到请求对象
+        ServerHttpRequest request = exchange.getRequest();
+        //请求路径
+        RequestPath path = request.getPath();
+        URI uri = request.getURI();
+        System.out.println("获取uri:"+uri.toString());
+        System.out.println("获取path:"+path.toString());
+        //请求参数 localhost:10000/abc?name=haha&age=18&girlFriends[0]=王翠花&girlFriends[1]=刘翠花
+        MultiValueMap<String, String> queryParams = request.getQueryParams();
+        //key 是string value List<String>
+        //name=haha name=List<String>{"haha"}
+        //name[0]=haha&name[1]=gaga 不会像springmvc一样整理成list数据,只会单独封装
+        //query在当前方法的value值永远都是一个元素的list对象.
+        List<String> name = queryParams.get("name");
+        System.out.println("name值元素个数:"+name.size());
+        System.out.println("数据分别是:"+name.toString());
+        //jwt解析,获取的headers Authorization
+        //获取头数据,打印展示
+        HttpHeaders headers = request.getHeaders();
+        //从所有头中,获取一个名称的头
+        List<String> headerValues = headers.get("Accept");
+        System.out.println("当前头的元素个数:"+headerValues.size());
+        System.out.println(headerValues);
+        //request其它api
+        request.getId();
+        //能解析客户端ip
+        request.getRemoteAddress();
+        return chain.filter(exchange);
     }
 
     @Override
