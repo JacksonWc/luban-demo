ba18e6787a35b728bb61bedb2eb77d9db3fade51
hebaodan <19560122127@163.com>
2023-10-09 20:10:07 +0800

    使用分布式锁解决微服务多实例集群中的线程安全问题--新增订单

--- luban-demo-order/luban-demo-order-main/src/main/java/com/tarena/demo/luban/all/main/consumer/OrderAddConsumer.java
+++ luban-demo-order/luban-demo-order-main/src/main/java/com/tarena/demo/luban/all/main/consumer/OrderAddConsumer.java
@@ -6,7 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
@@ -14,12 +19,39 @@ import org.springframework.stereotype.Component;
 public class OrderAddConsumer implements RocketMQListener<OrderAddParam> {
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public void onMessage(OrderAddParam message) {
-        log.info("订单新增接收消费对象:{}",message);
-        for(int i=0;i<10;i++){
 
+        log.info("订单新增接收消费对象:{}",message);
+        //生成 锁的key值 和随机value值
+        String lockKey="order:add:"+message.getOrderSn()+".lock";
+        String rand= UUID.randomUUID().toString();
+        //执行抢锁 api 底层 setnx+expire
+        ValueOperations<String, String> opsForValue = stringRedisTemplate.opsForValue();
+        //setnx lockKey rand && expire lockKey 10
+        Boolean tryLock = opsForValue.setIfAbsent(lockKey, rand, 10, TimeUnit.SECONDS);
+        log.info("抢锁结果:{}",tryLock);
+        //没有抢到锁
+        //循环只要tryLock是false 一直执行 抢锁 判断
+        while(!tryLock){
+            log.info("抢锁失败 等待五秒");
+            //等待5秒钟
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            tryLock = opsForValue.setIfAbsent(lockKey, rand, 10, TimeUnit.SECONDS);
         }
         orderService.addOrder(message);
+        //手动释放 判断是否是当前线程的锁
+        String lockValue = opsForValue.get(lockKey);
+        if (lockKey!=null && lockValue.equals(rand)){
+            //说明当前锁 是可以被释放的
+            stringRedisTemplate.delete(lockKey);
+        }
     }
 }
